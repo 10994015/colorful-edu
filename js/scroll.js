cee1f1311c998922fe869d2f4164426693484368
10994015 <a0938599191@gmail.com>
2022-08-05 02:39:13 +0800

    0805

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,6 @@ const cardbox = document.getElementsByClassName('cardbox')[0];
 const partners = document.getElementsByClassName('partners')[0];
 const course_content = document.getElementById('course').getElementsByClassName('content')[0]
 window.addEventListener('scroll',()=>{
-    
     if(this.scrollY>200){
         course_content.classList.add('fadeout');
     }
